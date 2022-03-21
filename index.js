9d0fd8638991c30fd8e955efed4b72f8ddc2b2bf
monalisadacoder <nangellalisa@gmail.com>
2022-03-21 12:13:19 +0300

    javascript intro

---
+++ index.js
@@ -0,0 +1,11 @@
+//manipulates behavior for index page
+let postTitle = "How to center headings on a page?";
+let postContent = "Lorem ipsum dolor sit,\
+ amet consectetur adipisicing elit. Repudiandae, \
+ numquam, omnis eveniet animi in dolorem aperiam\
+  fugit vitae deleniti consectetur aut, optio \
+  vel qui labore excepturi repellendus quam mollitia?\
+   Cum eligendi doloremque ad, maxime dolorem quia quod \
+   aliquam tenetur odit."
+let postAuthor = "Monalisa";
+console.log(postTitle,"*****", postContent, "****",postAuthor)
