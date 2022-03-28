a33ad9e8f24025f48b6cbb49fff923c2dde4632e
monalisadacoder <nangellalisa@gmail.com>
2022-03-28 12:45:24 +0300

    new assignment

---
+++ condition.js
@@ -0,0 +1,12 @@
+let temp = 70
+if (temp>=20&& temp<=36){
+    console.log("normal body temperature");
+
+}
+else if(temp>=37&& temp<=40){
+    console.log("cant believe u still alive ");
+
+}
+else{
+    console.log("hows the ghost town");
+}
